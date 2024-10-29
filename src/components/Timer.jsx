8499df341cc69715d6e44dfcfb6aecb01dcf252a
Ilija Ilic <ilija.ilic@elfak.rs>
2024-10-29 20:28:19 +0100

    add: time progress

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,19 +1,51 @@
+/* eslint-disable react/prop-types */
 
+import { useEffect } from 'react'
 import '../css/Timer.css'
 
-const Timer = () => {
+
+
+const Timer = ({title, endTime, elapsedTime}) => {
+
+  
+  useEffect(() => {
+    console.log(title, endTime, elapsedTime)
+  },[])
+
   return (
     <div className='Timer'>
-        <div className='inside-Circle'>
-            <p className='title-left'>Title from Props</p>
-            <p className='time'>00:00</p>
-            <p className='title-left'>25:00 left</p>
+      <div className='circles'>
+        <div className='outside-Circle' />
+
+        <div className={'half-Circle'} style={{zIndex: 4, rotate: '-90deg'}}>
+          <div className={'firstPart'+ ' ' + 'fixedPart'} style={ {backgroundColor: '#545576'}} />
+          <div className='secondPart'/> 
         </div>
+
+        <div className={'half-Circle' + ' ' + 'rotating-anim'}  style={{zIndex: 3 , rotate: '-90deg', animationDuration: endTime}}>
+          <div className='firstPart' style={ {backgroundColor: '#67cb88'}} />
+          <div className='secondPart'/> 
+        </div>
+
+        <div className={'half-Circle' + ' ' + 'rotating-anim2'}  style={{zIndex: 2 , rotate: '-90deg', animationDuration: endTime}}>
+          <div className='firstPart' style={ {backgroundColor: '#67cb88'}} />
+          <div className='secondPart'/> 
+        </div>
+
+        <div className='inside-Circle'>     
+          <p className='title-left'>{title}</p>
+          <p className='time'>00:00</p>
+          <p className='title-left'>{endTime} left</p>
+        </div>
+
+      </div>
+
         <div className='button-group'>
-            <button className='buttons' onClick={console.log('Start') }>Start</button>  
-            <button className='buttons' onClick={console.log('Pause')}>Pause</button>
-            <button className='buttons' onClick={console.log('Reset')}>Reset</button>
+          <button className='buttons'>Start</button>  
+          <button className='buttons'>Pause</button>
+           <button className='buttons'>Reset</button>
         </div>
+        
     </div>
   )
 }
--- src/timerApp.jsx
+++ src/timerApp.jsx
@@ -3,7 +3,7 @@ import './TimerApp.css'
 
 const TimerApp = () => {
   return (
-    <div className='TimerApp'><Timer/></div>
+    <div className='TimerApp'><Timer title = 'Moj Tajmer' endTime = {'10s'} elapsedTime = {0}/></div>
   )
 }
 
